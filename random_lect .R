d29a04824f1d8d09e20d0b83ff80c887ed3f3a0e
Philippe MICHEL <philippe@docteur-michel.fr>
2025-04-28 15:37:42 +0200

    Table de randomisation des experts

--- random_lect .R
+++ random_lect .R
@@ -2,12 +2,25 @@
 
 #  ------------------------------------------------------------------------
 #
-# Title : 
-#    By : 
+# Title : Randomisation des relectures
+#    By : PhM
 #  Date : 2025-04-24
-#    
+#
 #  ------------------------------------------------------------------------
 
+# Table de randomisation des relecture afin d'avoir un tirage aléatoire pour deux relecteurs sur trois participants par examen.
 
 
+# 1. Chargement des packages ------------------------------------------------
+library(tidyverse)
+library(readODS)
+# 2. Creation de la table ---------------------------------------------------
 
+rr <- c("AB", "AC", "BC")
+zz <- sample(rr,210,replace = T)
+nn <- 1:210
+zz <- as.factor(zz)
+zz <- tibble(nn,zz)
+names(zz) <- c("num_patient", "relecteurs")
+
+write_ods(zz, path = "relecteurs.ods", sheet = "relecteurs")
