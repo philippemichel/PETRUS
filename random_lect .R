b5dc1c45e44e48e3e62fffae095c263ba18e3e4f
Philippe MICHEL <philippe@docteur-michel.fr>
2025-04-29 10:44:56 +0200

    Relecteurs par centre

--- random_lect .R
+++ random_lect .R
@@ -16,11 +16,31 @@ library(tidyverse)
 library(readODS)
 # 2. Creation de la table ---------------------------------------------------
 
-rr <- c("AB", "AC", "BC")
-zz <- sample(rr,210,replace = T)
-nn <- 1:210
+rr <- c("Dr Al Tabaa, Dr Espita", "Dr Al Tabaa, Dr Ottaviani", "Dr Espita, Dr Ottaviani")
+n <- 40
+zz <- sample(rr,n,replace = T)
+nn <- 1:n
 zz <- as.factor(zz)
 zz <- tibble(nn,zz)
 names(zz) <- c("num_patient", "relecteurs")
 
-write_ods(zz, path = "relecteurs.ods", sheet = "relecteurs")
+# write_ods(zz, path = "relecteurs.ods", sheet = "relecteurs")
+
+centres <- tibble(nn)
+
+for(centre in 1:9){
+  nom <- paste0("Centre 0",centre)
+  if (centre == 1){zz <- rep("Dr Espita, Dr Ottaviani",n)}
+  else{
+      if (centre == 7){zz <- rep("Dr Al Tabaa, Dr Ottaviani",n)}
+    else{
+      if (centre == 9){zz <- rep("Dr Al Tabaa, Dr Espita",n)}
+      else{zz <- sample(rr,n,replace = T)}}}
+ zz <- as.factor(zz)
+ zz <- tibble(zz)
+ names(zz) <- nom
+ centres <- cbind(centres,zz)
+}
+
+
+
