ff66959dae2c1ee7b07eab56abe8a78426166b8d
Philippe MICHEL <philippe@docteur-michel.fr>
2025-08-14 14:25:25 +0200

    VALIDATION ok

--- Validation_lecteurs/essais.R
+++ Validation_lecteurs/essais.R
@@ -1,28 +1,19 @@
-form <- tt |>
-  dplyr::select(id, nom, starts_with("formation")) |>
-  pivot_longer(cols = 3:6, names_to = "nerf", values_to = "formation") |>
-  mutate(nerf = str_sub(nerf, 11))
-
-expert <- tt |>
-  dplyr::select(id, nom, starts_with("expert")) |>
-  pivot_longer(cols = 3:14, names_to = "nerf2", values_to = "experts") |>
-  mutate(nerf2 = str_sub(nerf2, 10))
+expt <- function(df = tt, coupe) {
+  zz <- tt |>
+    dplyr::select(nom, ends_with(coupe))
+  names(zz) <- c("nom", "form", "a", "b", "c")
+  zz |>
+    rowwise() |>
+    mutate(mes_exp = mean(c(a, b, c), na.rm = TRUE))
+}
 
 coupes <- c("nerf_dt", "gaine_dt", "nerf_gch", "gaine_gch")
-zz <- tt |>
-  dplyr::select(id, starts_with("expert"))
-nc <- 14
-for (nn in coupes) {
-  zz <- zz |>
-    rowwise() |>
-    mutate(xx = mean(c_across(ends_with(nn)), na.rm = TRUE)) |>
-    ungroup()
-  colnames(zz)[nc] <- paste0("moy_", nn)
-  nc <- nc + 1
+tbdiff <- NULL
+for (i in coupes) {
+  zz <- expt(tt, i)
+  tbdiff <- rbind(tbdiff, zz)
 }
-expx <- zz |>
-  dplyr::select(!starts_with("expert")) |>
-  pivot_longer(cols = 2:5, values_to = "experts")
-
-tot <- form |>
-  right_join(expx, by = "id")
+#
+tbdiff <- tbdiff |>
+  rowwise() |>
+  mutate(valmes = if_else(abs(form - mes_exp) > 1, 0, 1))
